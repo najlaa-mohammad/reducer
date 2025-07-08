46c244f861bbd0b2ada9ea724d4f5cf88067f03a
najlaa-mohammad <najlaaajore2@gmail.com>
2025-07-08 17:58:03 +0300

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,85 @@
 // import { useReducer } from "react";
 
+import { useEffect, useReducer } from "react";
 import "./App.css";
 import ActionSwitch from "./components/ActionSwitch";
 import ComponentA from "./components/ComponentA";
 import ComponentB from "./components/ComponentB";
 import ComponentC from "./components/ComponentC";
-import { useCountContext } from "./components/Context/CountContext";
+import axios from "axios";
+
+// import { useCountContext } from "./components/Context/CountContext";
+
+const initialState = {
+  loading: false,
+  posts: [],
+  error: "",
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_PENDING":
+      return {
+        loading: true,
+        error: "",
+        posts: [],
+      };
+    case "FETCH_SUCCESS":
+      return {
+        loading: false,
+        error: "",
+        posts: action.payload,
+      };
+    case "FETCH_ERROR":
+      return {
+        loading: false,
+        error: action.payload?.message || "Something went wrong",
+        posts: [],
+      };
+    default:
+      return state;
+  }
+};
 
 function App() {
-  const { count } = useCountContext();
+  const [state, dispatch] = useReducer(reducer, initialState);
+  // const { count } = useCountContext();
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        dispatch({ type: "FETCH_PENDING" });
+        const res = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
 
+        if (res.status === 200)
+          dispatch({ type: "FETCH_SUCCESS", payload: res.data });
+        res.data;
+        console.log(res.data);
+      } catch (error) {
+        dispatch({ type: "FETCH_ERROR", payload: error });
+        console.log(error);
+      }
+    };
+    getPosts();
+  }, []);
   return (
     <div>
-      <h1>Count - {count} </h1>
-      {/* <ActionSwitch/> */} 
-      <ComponentA />
+      {state.loading && <p>loading...</p>}
+      {!state.loading && state.error && <p>error:{state.error}</p>}
+      {!state.loading && state.posts && (
+        <ul>
+          {state.posts.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      )}
+
+      <h1>hello</h1>
+      {/* <h1>Count - {count} </h1> */}
+      {/* <ActionSwitch/> */}
+      {/* <ComponentA />
       <ComponentB />
-      <ComponentC />
+      <ComponentC /> */}
     </div>
   );
 }
